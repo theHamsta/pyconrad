b0aeb3951f46f1f7cac134c01cb4bd7f441a4a1d
runet01 <christopher.syben@fau.de>
2017-08-15 11:41:08 +0200

    added GridTest to testing Grid2D behaviour

---
+++ GridTest.py
@@ -0,0 +1,23 @@
+#from jpype import *
+import pyrad
+
+conrad = pyrad.getInstance()
+conrad.setup()
+#conrad.startReconstructionFilterPipeline()
+conrad.startConrad()
+
+grid = conrad.classes.stanford.rsl.conrad.data.numeric.Grid2D(30,30)
+grid.show("Grid2D Python")
+
+testClass = conrad.classes.stanford.rsl.science.syben.pytonTest.PythonTest
+testClassInstance = testClass()
+
+testClass.gridParameterStatic(grid)
+returnedGridStatic = testClass.gridParameterStaticWithReturn(grid)
+returnedGridStatic.show("gridParameterStaticWithReturn Python")
+print("Grid value is increased by 200 in static java method")
+
+testClassInstance.gridParameter(grid)
+returnedGrid = testClassInstance.gridParameterWithReturn(grid)
+returnedGrid.show("gridParameterWithReturn Python")
+print("Grid value is increased by 200 in java method")
