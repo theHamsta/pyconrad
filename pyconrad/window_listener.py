180970824a0553d4d2a3d4351d401fb8d8e8029d
syben <christopher.syben@fau.de>
2017-08-16 17:09:54 +0200

    added wnidow_listener overriding the awt windowclosing event to call pyCONRAD.stopGUI() method

---
+++ pyconrad/window_listener.py
@@ -0,0 +1,31 @@
+import pyconrad as pr
+import jpype.awt.event.WindowAdapter as wa
+
+class WindowListener(wa):
+
+    def __init__(self, *mth, **kw) :
+        object.__init__(self)
+
+        for i, j in kw.items() :
+            setattr(self, i, j)
+
+    def windowActivated(self, e) :
+        pass
+
+    def windowClosed(self,  e) :
+        pass
+
+    def windowDeactivated(self,  e) :
+        pass
+
+    def windowDeiconified(self,  e) :
+        pass
+
+    def windowIconified(self,  e) :
+        pass
+
+    def windowOpened(self,  e) :
+        pass
+
+    def windowClosing(self, e):
+        pr.pyCONRAD.getInstance().stopGui()
