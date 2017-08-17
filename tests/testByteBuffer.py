f96aa8dde9f3b3fab3bcdbe63e99c9ee1b5bbbc2
Stephan Seitz <stephan.seitz@fau.de>
2017-08-17 10:19:18 +0200

    testByteBuffer now works. But not as expected (Java uses Big Endian for float, my machine is Little Endian)

--- tests/testByteBuffer.py
+++ tests/testByteBuffer.py
@@ -1,29 +1,43 @@
-#import sys
-#sys.path.append('/localhome/local/projects/pyCONRAD/setup')
+# import sys
+# sys.path.append('/localhome/local/projects/pyCONRAD/setup')
 
 from jpype import *
 from pyconrad.pyCONRAD import *
 import numpy as np
 from pyconrad.ImageUtils import ImageUtil
+import struct
+
+## Grid2D.java should have the following method
+# public static Grid2D createGrid2D(ByteBuffer b, int width, int height ) {
+#     float[] fArray = new float[width * height];
+#     b.asFloatBuffer().get(fArray);
+#     return new Grid2D(fArray, width, height);
+# }
+
+
+def floatToBits(f):
+    s = struct.pack('>f', f)
+    return struct.unpack('>l', s)[0]
+
 
 jvm = PyConrad()
 jvm.setup()
 
-numpyIn = np.random.rand(423,23)
-numpyIn = numpyIn.astype(np.float32)
-
+numpyIn = np.ones([2, 23], np.float32)
 # grid1 =ImageUtil.wrapNumpyArrayToGrid2D(numpyIn)
 
 
 jByteBuffer = nio.convertToDirectBuffer(numpyIn.data)
-# grid = jvm.classes.stanford.rsl.conrad.data.numeric.Grid2D(numpyIn.shape[1],numpyIn.shape[0])
-grid = jvm.classes.stanford.rsl.conrad.data.numeric.Grid2D(jByteBuffer, JArray(JInt,1)(numpyIn.shape))
+grid = jvm.classes.stanford.rsl.conrad.data.numeric.Grid2D.createGrid2D(jByteBuffer, numpyIn.shape[1], numpyIn.shape[0])
 
 numpyOut = ImageUtil.wrapGrid2D(grid)
-assert np.allclose(numpyIn, numpyOut)
-
+print('in')
+print(numpyIn)
+print('out')
+print(numpyOut)
+# Java uses Big Endian, my machine is Little Endian
+print('Python: %x' % floatToBits(numpyIn[0,0]))
+print('Java: %x' % floatToBits(numpyOut[0,0]))
+# assert np.allclose(numpyIn, numpyOut)
 
 jvm.terminate()
-
-
-
