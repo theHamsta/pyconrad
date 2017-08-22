3590e478a599337fccbc40e549974acf4714d6ab
Christopher Syben <christopher.syben@fau.de>
2017-08-22 11:37:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def setup_package():
            packages=['pyconrad'],
            author='xxx',
            install_requires=[
-               'jpype1','numpy', 'pathlib', 'pyconrad_java'
+               'jpype1','numpy', 'pathlib', 'pyconrad_java>=0.0.1'
            ],
            dependency_links=[
                "git+https://git5.cs.fau.de/PyConrad/pyconrad_java/repository/archive.tar.gz?ref=0.0.1#egg=pyconrad_java"
