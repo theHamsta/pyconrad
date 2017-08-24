bf9468254468b9b2797ecd27c17b2e635d528469
Mario Amrehn <mario.amrehn@fau.de>
2017-08-24 12:36:42 +0200

    Minor consistency providing formatting changes.

--- pyconrad/_pyconradfabric.py
+++ pyconrad/_pyconradfabric.py
@@ -1,11 +1,11 @@
-from jpype import *
+from jpype import JArray, JDouble, JInt
 from ._pyconrad import PyConrad
 
 def makeSimpleVector(vec):
-    return PyConrad()['PyConradFabric'].makeSimpleVector(JArray(JDouble)(vec))
+    return PyConrad()["PyConradFabric"].makeSimpleVector(JArray(JDouble)(vec))
 
 def makePointND(vec):
-    return PyConrad()['PyConradFabric'].makePointND(JArray(JDouble)(vec))
+    return PyConrad()["PyConradFabric"].makePointND(JArray(JDouble)(vec))
 
 def makeCameraAxisDirection(val):
-    return PyConrad()['PyConradFabric'].makeCameraAxisDirection(JInt(val))
+    return PyConrad()["PyConradFabric"].makeCameraAxisDirection(JInt(val))
