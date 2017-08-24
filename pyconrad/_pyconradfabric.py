01b3ece00bc01d87a55d6559d11ffc06cf6ba158
runet01 <christopher.syben@fau.de>
2017-08-24 11:30:59 +0200

    added _pyconradfabric.py which contains fabric methods for PointND and simpleVector to hide the JArray(JDouble) casts from the user.

---
+++ pyconrad/_pyconradfabric.py
@@ -0,0 +1,11 @@
+from jpype import *
+from ._pyconrad import PyConrad
+
+def makeSimpleVector(vec):
+    return PyConrad()['PyConradFabric'].makeSimpleVector(JArray(JDouble)(vec))
+
+def makePointND(vec):
+    return PyConrad()['PyConradFabric'].makePointND(JArray(JDouble)(vec))
+
+def makeCameraAxisDirection(val):
+    return PyConrad()['PyConradFabric'].makeCameraAxisDirection(JInt(val))
