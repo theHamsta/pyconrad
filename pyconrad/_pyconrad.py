74b6ff20015f67193a2ff5ce81bbb38c814c3a74
Mario Amrehn <mario.amrehn@fau.de>
2017-08-24 12:34:59 +0200

    Formatting and minor changes.
     - auto PEP8 formatting
     - change classmethod without self reference to static
     - exception type specification.

--- pyconrad/_pyconrad.py
+++ pyconrad/_pyconrad.py
@@ -2,24 +2,24 @@
 # Copyright (C) 2010-2017 - Andreas Maier
 # CONRAD is developed as an Open Source project under the GNU General Public License (GPL-3.0)
 
-from jpype import startJVM, shutdownJVM, getDefaultJVMPath, isJVMStarted, JPackage, java
-from jpype import attachThreadToJVM, detachThreadFromJVM, JException, JProxy, JClass, JDouble, JArray
+import os
 import threading
 import time
-import os
-from . import _windowlistener as wl
-import pyconrad_java
 from pathlib import Path
+
+import pyconrad_java
+from jpype import attachThreadToJVM, detachThreadFromJVM, JException, JProxy, JClass, JDouble, JArray
+from jpype import startJVM, shutdownJVM, getDefaultJVMPath, isJVMStarted, JPackage, java
+
+from . import _windowlistener as wl
 from ._constructorproxies import pointNdConstructor, simpleVectorConstructor
 
 module_path = os.path.dirname(__file__)
 
 
-
 class PyConrad:
-
-    #Namespaces
-    classes, ij, java = None,None,None
+    # Namespaces
+    classes, ij, java = None, None, None
 
     __conrad_path = None
     __conrad_repo_set = None
@@ -32,13 +32,11 @@ class PyConrad:
 
     __imported_namespaces = []
 
-
     def __new__(cls, *args, **kwargs):
         if not cls.___instance:
             cls.___instance = super(PyConrad, cls).__new__(
                 cls, *args, **kwargs)
         return cls.___instance
-
 
     @staticmethod
     def get_instance():
@@ -46,16 +44,16 @@ class PyConrad:
             PyConrad.___instance = PyConrad()
         return PyConrad.___instance
 
-    def setup(self, max_ram = '8G', min_ram = '7G', dev_dirs = []):
+    def setup(self, max_ram="8G", min_ram="7G", dev_dirs=[]):
         if not self.is_java_initalized():
             try:
                 curr_directory = os.getcwd()
                 conrad_source_and_libs = self.__import__libs(dev_dirs)
                 os.chdir(self.__conrad_path)
-                startJVM(getDefaultJVMPath(), conrad_source_and_libs, "-Xmx%s" % max_ram, "-Xmn%s" % min_ram )
+                startJVM(getDefaultJVMPath(), conrad_source_and_libs, "-Xmx%s" % max_ram, "-Xmn%s" % min_ram)
                 os.chdir(curr_directory)
-                self.classes = JPackage('edu')
-                self.ij = JPackage('ij')
+                self.classes = JPackage("edu")
+                self.ij = JPackage("ij")
                 self.java = java
             except JException as ex:
                 print(ex)
@@ -80,7 +78,8 @@ class PyConrad:
         else:
             print("Some GUI is already started")
 
-    def is_java_initalized(self):
+    @staticmethod
+    def is_java_initalized():
         return isJVMStarted()
 
     def __stop_gui(self):
@@ -94,7 +93,7 @@ class PyConrad:
         proxy = JProxy("java.awt.event.WindowListener", inst=listener)
         self.__gui_instance.ReconstructionPipelineFrame.startConrad(proxy)
         self.__is_gui_started = True
-        print('Gui started', self.__gui_instance)
+        print("Gui started", self.__gui_instance)
         detachThreadFromJVM()
         while self.__is_gui_started:
             time.sleep(1)
@@ -106,7 +105,7 @@ class PyConrad:
         proxy = JProxy("java.awt.event.WindowListener", inst=listener)
         self.__gui_instance.CONRAD.setup(proxy)
         self.__is_gui_started = True
-        print('Gui started', self.__gui_instance)
+        print("Gui started", self.__gui_instance)
         detachThreadFromJVM()
         while self.__is_gui_started:
             time.sleep(1)
@@ -122,32 +121,32 @@ class PyConrad:
         # list directories, check whether CONRAD/RSL are there
         self.conrad_repo_set = False
 
-        extra_libs = ''
+        extra_libs = ""
         dev_src = []
         for dev in dev_dirs:
             dev_path = Path(dev)
-            dev_src.append(dev_path.joinpath('src'))
+            dev_src.append(dev_path.joinpath("src"))
             if dev_path.match("CONRAD"):
                 self.__conrad_path = dev_path
                 self.__conrad_repo_set = True
-                dev_lib = dev_path.joinpath('lib')
-                dev_classes = dev_path.joinpath('classes', 'production', 'CONRAD')
+                dev_lib = dev_path.joinpath("lib")
+                dev_classes = dev_path.joinpath("classes", "production", "CONRAD")
                 os.chdir(dev_path)
-                extra_libs = (dev_lib.joinpath(fn) for fn in dev_lib.iterdir() if '.jar' == fn.suffix)
-                extra_libs = ';'.join(map(str, [dev_classes, *extra_libs]))
+                extra_libs = (dev_lib.joinpath(fn) for fn in dev_lib.iterdir() if ".jar" == fn.suffix)
+                extra_libs = ";".join(map(str, [dev_classes, *extra_libs]))
 
         if self.__conrad_repo_set:
-            src = ';'.join(map(str, dev_src))
-            s = '-Djava.class.path=%s;%s' % (src, extra_libs)
+            src = ";".join(map(str, dev_src))
+            s = "-Djava.class.path=%s;%s" % (src, extra_libs)
         else:
             self.__conrad_path = pyconrad_java.conrad_jar_dir
             dev_src.append(pyconrad_java.conrad_jar_path)
-            src = ';'.join(map(str, dev_src))
-            s = '-Djava.class.path=%s;%s' % (src, extra_libs)
+            src = ";".join(map(str, dev_src))
+            s = "-Djava.class.path=%s;%s" % (src, extra_libs)
 
-        #Unix-like systems use : instead of ; to separate classpaths
-        if os.name != 'nt':  # Windows
-            s = s.replace(';',':')
+        # Unix-like systems use : instead of ; to separate classpaths
+        if os.name != "nt":  # Windows
+            s = s.replace(";", ":")
         return s
 
     def terminate(self):
@@ -162,9 +161,9 @@ class PyConrad:
         self.__imported_namespaces.append(package_name)
 
     def __getitem__(self, classname):
-        if(classname == 'PointND'):
+        if "PointND" == classname:
             return pointNdConstructor
-        if(classname == 'SimpleVector'):
+        if "SimpleVector" == classname:
             return simpleVectorConstructor
 
         success = None
@@ -173,7 +172,7 @@ class PyConrad:
         try:
             rtn = JClass(classname)
             success = rtn
-        except:
+        except RuntimeError:
             pass
 
         # Imported namespaces
@@ -182,11 +181,11 @@ class PyConrad:
                 rtn = JClass(package + "." + classname)
                 success = rtn
                 break
-            except:
+            except RuntimeError:
                 pass
 
-        if success == None:
-            raise Exception('Class \"%s\" not found in the following namespaces:\n %s' % (classname,self.__imported_namespaces))
+        if success is None:
+            raise Exception("Class \"%s\" not found in the following namespaces:\n %s" % (classname, self.__imported_namespaces))
 
         return success
 
