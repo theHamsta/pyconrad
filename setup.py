d63ca1e69c1827c55e53f21f46382a78dde4c9b0
Stephan Seitz <stephan.seitz@fau.de>
2017-08-28 18:21:00 +0200

    - append directory of download_conrad_jar to sys.path (now working)

--- setup.py
+++ setup.py
@@ -18,8 +18,10 @@ from setuptools import setup
 # post install hint from:
 # https://stackoverflow.com/questions/17806485/execute-a-python-script-post-install-using-distutils-setuptools
 def _post_install(dir):
-    from pyconrad import download_conrad
     cwd=os.path.join(dir, 'pyconrad')
+    #os.path.append(os.path.join( os.path.dirname(__file__), 'pyconrad'))
+    sys.path.append(cwd)
+    import download_conrad
     download_conrad.download_conrad(cwd)
     print("CONRAD has been installed to %s" % download_conrad.conrad_jar_dir())
 
@@ -45,8 +47,8 @@ def setup_package():
                'jpype1','numpy', 'pathlib', 'urllib3'
            ],
            cmdclass={'install': install},
-           url='https://git5.cs.fau.de/PyConrad/pyCONRAD/',
-           download_url='https://git5.cs.fau.de/PyConrad/pyCONRAD/repository/archive.tar.gz?ref=0.0.1')
+           url='https://git5.cs.fau.de/PyConrad/pyCONRAD/'
+           )
         #setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
           #use_pyscaffold=True)
 
