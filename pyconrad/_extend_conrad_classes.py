0e43de392f1ac2676a2b8b68113effeb810e0cf1
Stephan Seitz <stephan.lauf@yahoo.de>
2017-08-30 18:08:05 +0200

    - restructure _extend_conrad_classes.py

--- pyconrad/_extend_conrad_classes.py
+++ pyconrad/_extend_conrad_classes.py
@@ -2,10 +2,19 @@ from jpype import *
 import pyconrad
 import numpy as np
 
-def numpy_pointnd(self):
-    return np.array(self.getCoordinates()[:])
+def _extend_pointnd():
+    @classmethod
+    def _pointnd_from_numpy(cls, array):
+        return cls(JArray(JDouble)(array))
 
+    def _pointnd_numpy(self):
+        return np.array(self.getCoordinates()[:])
+
+    pointnd_class = pyconrad.PyConrad().classes.stanford.rsl.conrad.geometry.shapes.simple.PointND
+    pointnd_class.numpy = _pointnd_numpy
+    pointnd_class.from_numpy = _pointnd_from_numpy
 
 def extend_all_classes():
-   pointnd_class = pyconrad.PyConrad().classes.stanford.rsl.conrad.geometry.shapes.simple.PointND
-   pointnd_class.numpy = numpy_pointnd
+    _extend_pointnd()
+
+
