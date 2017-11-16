002963e678993b8aab025a0ed20ea25d9c4b7b5a
Stephan Seitz <stephan.lauf@yahoo.de>
2017-11-16 12:47:59 +0100

    - don't start GUI in pyconrad.autoinit

--- pyconrad/autoinit.py
+++ pyconrad/autoinit.py
@@ -3,6 +3,6 @@ import pyconrad._pyconrad
 
 if not pyconrad._pyconrad.PyConrad().is_initialized:
     pyconrad.setup_pyconrad()
-
-if not  pyconrad._pyconrad.PyConrad().is_gui_started:
-    pyconrad.start_conrad()
+#
+# if not pyconrad._pyconrad.PyConrad().is_gui_started:
+#     pyconrad.start_conrad()
