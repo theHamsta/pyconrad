7fe843a50262c45f9c149eaacdf53cd5214d5b6d
Stephan Seitz <stephan.lauf@yahoo.de>
2017-11-23 16:35:07 +0100

    - make pytest working again

--- setup.py
+++ setup.py
@@ -15,8 +15,10 @@ from setuptools.command.install import _install
 
 # post install hint from:
 # https://stackoverflow.com/questions/17806485/execute-a-python-script-post-install-using-distutils-setuptools
+
+
 def _post_install(dir):
-    cwd=os.path.join(dir, 'pyconrad')
+    cwd = os.path.join(dir, 'pyconrad')
     #os.path.append(os.path.join( os.path.dirname(__file__), 'pyconrad'))
     sys.path.append(cwd)
     import download_conrad
@@ -30,6 +32,7 @@ class install(_install):
         self.execute(_post_install, (self.install_lib,),
                      msg="Installing Java dependencies...")
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
@@ -37,31 +40,30 @@ def read(fname):
 def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
-    setup( name='pyconrad',
-            version='0.0.6',
-            packages=['pyconrad'],
-            author='Andreas Maier',
-            author_email='andreas.maier@fau.de',
-            license ='GPL 3.0',
-            install_requires=[
-                'jpype1','numpy', 'pathlib', 'urllib3', 'pyevtk'
-                ],
-            cmdclass={'install': install},
-            url='https://git5.cs.fau.de/PyConrad/pyCONRAD/',
-           description='Python wrapper for CONRAD (https://www5.cs.fau.de/conrad/), a framework for cone beam radiography',
-            long_description=read('README.md'),
-            entry_points={
-                'console_scripts': [
-                    'pyconrad = pyconrad.__main__:main',
-                    ]
-                },
-                )
-        #setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
-          #use_pyscaffold=True)
-
+    setup(name='pyconrad',
+          version='0.0.6',
+          packages=['pyconrad'],
+          author='Andreas Maier',
+          author_email='andreas.maier@fau.de',
+          license='GPL 3.0',
+          install_requires=[
+               'jpype1', 'numpy', 'pathlib', 'urllib3', 'pyevtk', 'setuptools'
+          ],
+          cmdclass={'install': install},
+          url='https://git5.cs.fau.de/PyConrad/pyCONRAD/',
+          description='Python wrapper for CONRAD (https://www5.cs.fau.de/conrad/), a framework for cone beam radiography',
+          long_description=read('README.md'),
+          entry_points={
+              'console_scripts': [
+                  'pyconrad = pyconrad.__main__:main',
+              ]
+          },
+          setup_requires=['pytest-runner'],
+          tests_require=['pytest']
+          )
+    #setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
+    # use_pyscaffold=True)
 
 
 if __name__ == "__main__":
     setup_package()
-
-
