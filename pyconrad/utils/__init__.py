97f987abbbfe4ff914b2ffbae3744b50ae14adc9
Stephan Seitz <stephan.lauf@yahoo.de>
2017-11-24 16:18:36 +0100

    - move important config method to utils

---
+++ pyconrad/utils/__init__.py
@@ -0,0 +1,21 @@
+import pyconrad
+
+_ = pyconrad.ClassGetter(
+    'edu.stanford.rsl.conrad.utils',
+    'java.beans',
+    'java.io',
+    'edu.stanford.rsl.conrad.geometry.trajectories'
+)
+
+
+
+def get_conf() -> pyconrad.AutoCompleteConrad.edu.stanford.rsl.conrad.utils.Configuration:
+    conf = _.Configuration.getGlobalConfiguration()
+
+    return conf
+
+
+def get_geometry() -> pyconrad.AutoCompleteConrad.edu.stanford.rsl.conrad.geometry.trajectories.Trajectory:
+    # _.Configuration.loadConfiguration() #TODO load config on pyconrad startup
+    conf = _.Configuration.getGlobalConfiguration()
+    return geo
