3573a0621b236548895c95155722b7db2b5e815f
Stephan Seitz <stephan.lauf@yahoo.de>
2017-11-24 14:19:26 +0100

    - fix warning in _extend_conrad_classes.py

--- pyconrad/_extend_conrad_classes.py
+++ pyconrad/_extend_conrad_classes.py
@@ -10,6 +10,7 @@ from ._pygrid import PyGrid
 import numpy as np
 import warnings
 
+
 def _extend_pointnd():
     @classmethod
     def _pointnd_from_numpy(cls, array):
@@ -22,10 +23,12 @@ def _extend_pointnd():
     def _numpy_pointnd(self):
         return np.array(self.getCoordinates()[:])
 
-    pointnd_class = JPackage('edu').stanford.rsl.conrad.geometry.shapes.simple.PointND
+    pointnd_class = JPackage(
+        'edu').stanford.rsl.conrad.geometry.shapes.simple.PointND
     pointnd_class.as_numpy = _numpy_pointnd
     pointnd_class.from_numpy = _pointnd_from_numpy
     pointnd_class.from_list = _pointnd_from_list
+
 
 def _extend_simple_vector():
     @classmethod
@@ -39,15 +42,17 @@ def _extend_simple_vector():
     def _numpy_simple_vector(self):
         return np.array(self.copyAsDoubleArray())
 
-    simple_vector_class = JPackage('edu').stanford.rsl.conrad.numerics.SimpleVector
+    simple_vector_class = JPackage(
+        'edu').stanford.rsl.conrad.numerics.SimpleVector
     simple_vector_class.as_numpy = _numpy_simple_vector
     simple_vector_class.from_numpy = _simple_vector_from_numpy
     simple_vector_class.from_list = _simple_vector_from_list
 
+
 def _extend_simple_matrix():
     @classmethod
     def _simple_matrix_from_numpy(cls, array):
-        return cls(JArray(JDouble,np.ndim(array))(array.tolist()))
+        return cls(JArray(JDouble, np.ndim(array))(array.tolist()))
 
     @classmethod
     def _simple_matrix_from_list(cls, array):
@@ -56,27 +61,30 @@ def _extend_simple_matrix():
     def _numpy_simple_matrix(self):
         return np.matrix(self.copyAsDoubleArray())
 
-    simple_matrix_class = JPackage('edu').stanford.rsl.conrad.numerics.SimpleMatrix
+    simple_matrix_class = JPackage(
+        'edu').stanford.rsl.conrad.numerics.SimpleMatrix
     simple_matrix_class.as_numpy = _numpy_simple_matrix
     simple_matrix_class.from_numpy = _simple_matrix_from_numpy
     simple_matrix_class.from_list = _simple_matrix_from_list
 
+
 def _extend_numeric_grid():
 
     @classmethod
-    def _numeric_grid_from_numpy(cls,array):
-        if array.dtype == java_float_dtype:
-            warnings.warn('Array type is not java_float_dtype. Additional copy was needed for conversion')
-            return PyGrid.from_numpy(array).grid()
+    def _numeric_grid_from_numpy(cls, array):
+        if array.dtype != java_float_dtype:
+            warnings.warn(
+                'Array type is not java_float_dtype. Additional copy was needed for conversion')
+            return PyGrid.from_numpy(array).grid
         else:
-            return PyGrid.from_numpy(np.array(array,java_float_dtype)).grid
+            return PyGrid.from_numpy(np.array(array, java_float_dtype)).grid
 
     @classmethod
-    def _numeric_grid_from_list(cls,input_list):
-        return PyGrid.from_numpy(np.array(input_list,java_float_dtype)).grid
+    def _numeric_grid_from_list(cls, input_list):
+        return PyGrid.from_numpy(np.array(input_list, java_float_dtype)).grid
 
     @classmethod
-    def _numeric_grid_from_size(cls,*size):
+    def _numeric_grid_from_size(cls, *size):
         return PyGrid(list(size)).grid
 
     def _numpy_grid(self):
@@ -90,7 +98,8 @@ def _extend_numeric_grid():
             end = idxs.stop or self.getSize()[2]
             assert idxs.step == 1 or not idxs.step, "Only step==1 is supported"
 
-            rtn = self.type(reversed(*self.getSize()[:]),self.getsize()[1], end - start, False)
+            rtn = self.type(
+                reversed(*self.getSize()[:]), self.getsize()[1], end - start, False)
             # rtn = pyconrad.PyConrad().classes.stanford.rsl.conrad.data.numeric.Grid3D(self.getSize()[0],self.getSize()[1], end - start, False)
             rtn.setOrigin(self.getOrigin())
             rtn.setSpacing(self.getSpacing())
@@ -132,6 +141,7 @@ def _extend_numeric_grid():
     grid_class.__setitem__ = _numeric_grid_setitem
     grid_class.shape = _numeric_grid_shape
     grid_class.pygrid = _pygrid
+
 
 def extend_all_classes():
     _extend_pointnd()
