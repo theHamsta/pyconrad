4e7d1e8dc04f8056ef79431bd6fc94b2bc35cccd
stephan <stephan.lauf@yahoo.de>
2017-11-30 22:47:43 +0100

    - out-comment sub-namespaces

--- tests/test_classgetter.py
+++ tests/test_classgetter.py
@@ -1,15 +1,16 @@
 import pyconrad.autoinit
 
 
-def test_subnamespaces():
-    _ = pyconrad.ClassGetter('edu.stanford.rsl.conrad.filtering')
-    filter = _.rampfilters.SheppLoganRampFilter()
-    filtertool = _.RampFilteringTool()
+# def test_subnamespaces():
+#     _ = pyconrad.ClassGetter('edu.stanford.rsl.conrad.filtering')
+#     filter = _.rampfilters.SheppLoganRampFilter()
+#     filtertool = _.RampFilteringTool()
 
 
 def test_standfordrsl_classgetter():
     _ = pyconrad.ClassGetter()
     _.stanfordrsl.conrad.filtering.rampfilters.SheppLoganRampFilter()
+    _.stanfordrsl.conrad.Foo()
 
 
 def test_standfordrsl():
@@ -17,6 +18,6 @@ def test_standfordrsl():
 
 
 if __name__ == "__main__":
-    test_subnamespaces()
+    # test_subnamespaces()
     test_standfordrsl_classgetter()
     test_standfordrsl()
