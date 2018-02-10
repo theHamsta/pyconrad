15a7651157ee15f09f7e64b3bc2fce75df6e5374
stephan <stephan.lauf@yahoo.de>
2018-02-11 00:08:11 +0100

    - skip GUI tests on Travis

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -2,14 +2,18 @@ import warnings
 import pyconrad
 import time
 import pyconrad
+import os
+import pytest
 
 
+@pytest.mark.skipif("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "Skipping this test on Travis CI.")
 def test_basic_example():
     import pyconrad_examples.first_steps._1_basic_example
     pyconrad.ij().WindowManager.closeAllWindows()
     return
 
 
+@pytest.mark.skipif("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "Skipping this test on Travis CI.")
 def test_numpy_example():
     import pyconrad_examples.first_steps._3_numpy_and_conrad
     pyconrad.ij().WindowManager.closeAllWindows()
