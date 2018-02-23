b6c50b3c79741893f4ed382398ab5235a1cd1565
Stephan Seitz <stephan.lauf@yahoo.de>
2018-02-23 17:00:07 +0100

    - fix previous commit

--- tests/test_opencl.py
+++ tests/test_opencl.py
@@ -6,16 +6,15 @@ import os
 # import pyconrad.autoinit
 try:
     from pyconrad.opencl import *
+    if not pyconrad.is_initialized():
+        # pyconrad.setup_pyconrad(dev_dirs=['/home/stephan/projects/CONRAD'])
+        pyconrad.setup_pyconrad()
+
+    _ = pyconrad.ClassGetter('edu.stanford.rsl.tutorial.cone')
 except Exception as e:
     warnings.warn(str(e))
 import numpy as np
 import jpype
-
-if not pyconrad.is_initialized():
-    # pyconrad.setup_pyconrad(dev_dirs=['/home/stephan/projects/CONRAD'])
-    pyconrad.setup_pyconrad()
-
-_ = pyconrad.ClassGetter('edu.stanford.rsl.tutorial.cone')
 
 
 @pytest.mark.skipif("WITH_OPENCL" in os.environ and os.environ["WITH_OPENCL"] == "0", reason="Skipping this test on Travis CI.")
