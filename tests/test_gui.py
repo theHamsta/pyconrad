4de2d1f4912d3e9d017c537fe4792d04d6364e2c
stephan <stephan.lauf@yahoo.de>
2018-05-15 17:19:34 +0200

    Fix issue #12: Getting rid of the raw data opener

---
+++ tests/test_gui.py
@@ -0,0 +1,11 @@
+import pyconrad.autoinit
+import pytest
+
+
+@pytest.mark.skipif("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", reason="Skipping this test on Travis CI.")
+def test_start_gui():
+    pyconrad.start_gui()
+
+
+if __name__ == "__main__":
+    test_start_gui()
