e3dd31d3f1e9baf46da296310b192e0e05e51386
Stephan <stephan.lauf@yahoo.de>
2018-08-11 12:39:20 +0200

    More dynamic defaults for JVM memory allocation

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ def setup_package():
           author_email='andreas.maier@fau.de',
           license='GPL 3.0',
           install_requires=[
-                       'jpype1', 'numpy', 'pathlib', 'urllib3', 'pyevtk', 'setuptools', 'vtk'
+                       'jpype1', 'numpy', 'pathlib', 'urllib3', 'pyevtk', 'setuptools', 'vtk','tendo'
           ],
           extras_require={'opencl': ['pyopencl']},
           cmdclass={'install': install},
