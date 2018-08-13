0d1858f1d60cab3a53069c85cd9fc11ba7b841f0
Stephan Seitz <stephan.seitz@fau.de>
2018-08-13 17:27:44 +0200

    Add origin and spacing parameter to imshow

--- pyconrad_examples/visualization/imshow.py
+++ pyconrad_examples/visualization/imshow.py
@@ -6,7 +6,7 @@ import numpy as np
 print('Enjoy white noise!')
 for i in range(300):
     noise = np.random.rand(200, 200)
-    pyconrad.imshow(noise, 'White noise')
+    pyconrad.imshow(noise, 'White noise', spacing=[200, 2, 3], origin=[0, 2])
     time.sleep(0.01)
 
 pyconrad.close_all_windows()
