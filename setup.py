199f495f409c715901153797899a1f3d48d0e26d
Stephan Seitz <stephan.seitz@fau.de>
2018-08-13 07:35:42 +0200

    Implement single-instance conrad_imagej.py

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ def setup_package():
           author_email='andreas.maier@fau.de',
           license='GPL 3.0',
           install_requires=[
-                       'jpype1', 'numpy', 'pathlib', 'urllib3', 'pyevtk', 'setuptools', 'vtk','tendo'
+                       'jpype1', 'numpy', 'pathlib', 'urllib3', 'pyevtk', 'setuptools', 'vtk', 'procbridge'
           ],
           extras_require={'opencl': ['pyopencl']},
           cmdclass={'install': install},
