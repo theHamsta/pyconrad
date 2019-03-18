41945aa10e990d45e74ca8961446c2bdb7f94334
Stephan Seitz <stephan.seitz@fau.de>
2019-03-18 18:23:26 +0100

    Skip config test on CI

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,9 +2,14 @@
 
 """
 
-import pyconrad.autoinit
+import os
+
+import pytest
+
+import pyconrad.autoinit  # NOQA
 
 
+@pytest.mark.skipif("CI" in os.environ and os.environ["CI"] == "true", reason="No Conrad.xml on CI")
 def test_get_projection_matrices():
     import pyconrad.config
     matrices = pyconrad.config.get_projection_matrices()
