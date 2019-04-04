d85f04705b44f131394554bfc0aa41c0485bf276
Stephan Seitz <stephan.seitz@fau.de>
2019-04-04 11:52:29 +0200

    Bump version 0.4.1

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
     setup(name='pyconrad',
-          version='0.4',
+          version='0.4.1',
           packages=['pyconrad', 'pyconrad._autocomplete_files', 'pyconrad_examples'
                     ],
           author='Andreas Maier',
