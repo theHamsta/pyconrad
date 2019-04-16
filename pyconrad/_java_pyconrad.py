970a8eacc4166e546d998aa09262bbf5c4920f00
Stephan Seitz <stephan.seitz@fau.de>
2019-04-16 07:55:08 +0200

    Silence warnings in _java_pyconrad

--- pyconrad/_java_pyconrad.py
+++ pyconrad/_java_pyconrad.py
@@ -1,27 +1,28 @@
 import jpype
 
 
-
 class JavaPyConrad():
     def __init__(self):
         self.globals_dict = dict()
 
     def eval(self, object, code, args):
         if object.autoConvertConradGrids:
-            args = [(i.as_numpy() if jpype.JClass('edu.stanford.rsl.conrad.data.numeric.NumericGrid') else i) for i in args]
-        if not object in self.globals_dict:
+            args = [(i.as_numpy() if jpype.JClass('edu.stanford.rsl.conrad.data.numeric.NumericGrid') else i)
+                    for i in args]
+        if object not in self.globals_dict:
             self.globals_dict[object] = dict()
         locals().update({**self.globals_dict[object]})
         rtn = eval(code)
         if object.autoConvertConradGrids:
-            rtn = jpype.JClass('edu.stanford.rsl.conrad.data.numeric.NumericGrid').from_numpy(rtn) if hasattr(rtn,"__array__") else rtn
+            rtn = jpype.JClass('edu.stanford.rsl.conrad.data.numeric.NumericGrid').from_numpy(
+                rtn) if hasattr(rtn, "__array__") else rtn
         self.globals_dict[object].update(locals())
         return rtn
 
     def exec(self, object, code, args):
         if object.autoConvertConradGrids:
             args = [(i.as_numpy() if hasattr(object, "as_numpy") else i) for i in args]
-        if not object in self.globals_dict:
+        if object not in self.globals_dict:
             self.globals_dict[object] = dict()
         locals().update({**self.globals_dict[object]})
         exec(code)
