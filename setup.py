a83ff5ccb393f6bfcac6638d6bdae3f9c3cc97b0
Stephan Seitz <stephan.seitz@fau.de>
2019-09-26 14:10:56 +0200

    Bump version: 0.6.8

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ def read(fname):
 
 def setup_package():
     setup(name='pyconrad',
-          version='0.6.7',
+          version='0.6.8',
           packages=['pyconrad', 'pyconrad._autocomplete_files', 'pyconrad_examples'
                     ],
           author='Andreas Maier',
